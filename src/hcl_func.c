53748bb06124cce5b3a6da6cff32ae7ef4d8259b
nukosuke <nukosuke@lavabit.com>
2019-06-17 15:40:00 +0900

    Add Doxy comments

--- src/hcl_func.c
+++ src/hcl_func.c
@@ -1,3 +1,8 @@
+/**
+ * @file hcl_func.c
+ * @brief Implementation of functions for HCL function
+ * @author nukosuke
+ */
 #include <hcl/func.h>
 
 void hcl_def_func (char *name)
--- src/hcl_var.c
+++ src/hcl_var.c
@@ -1,3 +1,8 @@
+/**
+ * @file hcl_var.c
+ * @brief Implementation of functions for HCL variable
+ * @author nukosuke
+ */
 #include <hcl/var.h>
 
 void hcl_def_var (char *name)
