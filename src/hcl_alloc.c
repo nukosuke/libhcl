f1e65846f7b19d80d5b2a2ded7cd0b8891bc60e5
nukosuke <nukosuke@lavabit.com>
2019-06-18 15:53:49 +0900

    Add alloc

---
+++ src/hcl_alloc.c
@@ -0,0 +1,46 @@
+/**
+ * @file hcl_alloc.c
+ * @brief Implementation of memory allocation for HCL
+ * @author nukosuke
+ */
+#include "hcl_alloc.h"
+
+int hcl_string ()
+{
+  return 1;
+}
+
+int hcl_number ()
+{
+  return 1;
+}
+
+int hcl_bool ()
+{
+  return 1;
+}
+
+int hcl_list ()
+{
+  return 1;
+}
+
+int hcl_set ()
+{
+  return 1;
+}
+
+int hcl_map ()
+{
+  return 1;
+}
+
+int hcl_object ()
+{
+  return 1;
+}
+
+int hcl_tuple ()
+{
+  return 1;
+}
