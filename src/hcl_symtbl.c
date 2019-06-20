12988b26be3120553ac481f6ee9f2b48066f39ba
nukosuke <nukosuke@lavabit.com>
2019-06-20 15:02:05 +0900

    Impl allocator and destructor of symbol table

--- src/hcl_symtbl.c
+++ src/hcl_symtbl.c
@@ -3,27 +3,44 @@
  * @brief Implementation of HCL symbol table
  * @author nukosuke
  */
+#include <stdlib.h>
 #include "hcl_symtbl.h"
 
-struct hcl_symtbl_ent *hcl_symtbl_ent_new (void)
+/**
+ * Symbol table allocator
+ *
+ * @param size default size of the table
+ * @return Pointer of allocated table
+ */
+struct hcl_symtbl *hcl_symtbl_new (size_t size)
 {
-  return 0;
+  struct hcl_symtbl *tbl = (struct hcl_symtbl *) malloc (sizeof (struct hcl_symtbl));
+  if (tbl == NULL)
+    return NULL;
+
+  tbl->ent = (struct hcl_symtbl_ent *) malloc (sizeof (struct hcl_symtbl_ent) * size);
+  if (tbl->ent == NULL)
+    {
+      free (tbl);
+      return NULL;
+    }
+
+  tbl->size = size;
+  return tbl;
 }
 
-struct hcl_symtbl *hcl_symtbl_new (void)
-{
-  return 0;
-}
-
-void hcl_symtbl_ent_free (struct hcl_symtbl_ent *ent)
-{
-}
-
+/**
+ * Symbol table destructor
+ *
+ * @param tbl Pointer of the table to be deleted
+ */
 void hcl_symtbl_free (struct hcl_symtbl *tbl)
 {
+  free (tbl->ent);
+  free (tbl);
 }
 
-int hcl_symtbl_setent (struct hcl_symtbl_ent *ent, struct hcl_symtbl *tbl)
+int hcl_symtbl_addent (struct hcl_symtbl_ent *ent, struct hcl_symtbl *tbl)
 {
   return 1;
 }
@@ -31,4 +48,8 @@ int hcl_symtbl_setent (struct hcl_symtbl_ent *ent, struct hcl_symtbl *tbl)
 struct hcl_symtbl_ent *hcl_symtbl_getent (void)
 {
   return 0;
+}
+
+void hcl_symtbl_print (struct hcl_symtbl *tbl)
+{
 }
--- src/hcl_symtbl.h
+++ src/hcl_symtbl.h
@@ -5,6 +5,7 @@
  */
 #ifndef LIBHCL_HCL_SYMTBL_H
 #define LIBHCL_HCL_SYMTBL_H
+#include <stddef.h> // size_t
 
 enum hcl_type {
   HCL_STRING_T,
@@ -26,15 +27,14 @@ struct hcl_symtbl_ent
 struct hcl_symtbl
 {
   struct hcl_symtbl_ent *ent;
+  size_t size;
 };
 
-struct hcl_symtbl_ent *hcl_symtbl_ent_new (void);
-struct hcl_symtbl *hcl_symtbl_new (void);
-
-void hcl_symtbl_ent_free (struct hcl_symtbl_ent *);
+struct hcl_symtbl *hcl_symtbl_new (size_t);
+// TODO realloc funcs
 void hcl_symtbl_free (struct hcl_symtbl *);
 
-int hcl_symtbl_setent (struct hcl_symtbl_ent *, struct hcl_symtbl *);
+int hcl_symtbl_addent (struct hcl_symtbl_ent *, struct hcl_symtbl *);
 struct hcl_symtbl_ent *hcl_symtbl_getent (void);
 
 #endif /* LIBHCL_HCL_SYMTBL */
