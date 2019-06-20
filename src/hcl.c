042bd07d1eda252bc58c77c48c2ad9dd4e1e862c
nukosuke <nukosuke@lavabit.com>
2019-06-20 15:11:57 +0900

    Fix Doxygen comment

--- src/hcl.c
+++ src/hcl.c
@@ -7,18 +7,32 @@
 #include <hcl.h>
 #include <hcl/types.h>
 
+/**
+ * Allocator of HCL config object
+ */
 struct hcl_t *hcl_new (void)
 {
   struct hcl_t *hcl = (struct hcl_t *) malloc (sizeof (struct hcl_t));
   return hcl;
 }
 
+/**
+ * Destructor of HCL config object
+ *
+ * @param hcl Pointer of HCL config object
+ */
 void hcl_free (struct hcl_t *hcl)
 {
   if (hcl != NULL)
     free (hcl);
 }
 
+/**
+ * Initializer of HCL config object
+ *
+ * @param hcl Pointer of HCL config object
+ * @return Status of initialization
+ */
 int hcl_init (struct hcl_t *hcl)
 {
   return 1;
