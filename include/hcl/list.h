d97f29dcfe25996ebe9a56f59267c857564a7aff
nukosuke <nukosuke@lavabit.com>
2019-06-21 15:46:40 +0900

    Add allocator and destructor

--- include/hcl/list.h
+++ include/hcl/list.h
@@ -6,6 +6,7 @@
 #ifndef LIBHCL_HCL_LIST_H
 #define LIBHCL_HCL_LIST_H
 #include <stddef.h> // size_t
+#include <hcl/types.h>
 
 /**
  * HCL list type
@@ -13,6 +14,7 @@
 struct hcl_list
 {
   struct hcl_list_ent *head;
+  struct hcl_list_ent *tail;
   size_t len;
 };
 
@@ -22,9 +24,16 @@ struct hcl_list
 struct hcl_list_ent
 {
   struct hcl_list_ent *next;
+  enum hcl_type type;
   void *addr;
 };
 
 struct hcl_list *hcl_list_new (void);
+void hcl_list_free (struct hcl_list *);
+
+int hcl_list_addent (struct hcl_list *, void *, enum hcl_type);
+
+static struct hcl_list_ent *hcl_list_ent_new (void *addr, enum hcl_type type);
+static void hcl_list_ent_free (struct hcl_list_ent *);
 
 #endif /* LIBHCL_HCL_LIST_H */
--- src/hcl_list.c
+++ src/hcl_list.c
@@ -39,3 +39,51 @@ void hcl_list_free (struct hcl_list *list)
     }
   free (list);
 }
+
+/**
+ * Add entry to HCL list
+ *
+ * @param list Pointer of HCL list
+ * @param addr Address of value data
+ * @param type HCL type
+ * @return Status of addition
+ */
+int hcl_list_addent (struct hcl_list *list, void *addr, enum hcl_type type)
+{
+  if ((list->tail->next = hcl_list_ent_new (addr, type)) == NULL)
+    return 1;
+
+  list->tail = list->tail->next;
+  return 0;
+}
+
+/**
+ * Allocator of HCL list entry
+ *
+ * @param addr Address of value data
+ * @param type HCL type
+ * @return Pointer of allocated HCL list entry
+ */
+static struct hcl_list_ent *hcl_list_ent_new (void *addr, enum hcl_type type)
+{
+  struct hcl_list_ent *ent = (struct hcl_list_ent *) malloc (sizeof (struct hcl_list_ent));
+  if (ent == NULL)
+    return NULL;
+
+  ent->next = NULL;
+  ent->addr = addr;
+  ent->type = type;
+
+  return ent;
+}
+
+/**
+ * Destructor of HCL list entry
+ *
+ * @param ent Pointer of HCL list entry
+ */
+static void hcl_list_ent_free (struct hcl_list_ent *ent)
+{
+  free (ent->addr);
+  free (ent);
+}
