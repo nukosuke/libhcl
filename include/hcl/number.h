db6f517ba644a41c69a4a0327cb90ab7bef54f9a
nukosuke <nukosuke@lavabit.com>
2019-06-24 14:09:13 +0900

    Add hcl_number func

--- include/hcl/number.h
+++ include/hcl/number.h
@@ -14,4 +14,7 @@ struct hcl_number
   // TODO
 };
 
+struct hcl_number *hcl_number_new (void);
+void hcl_number_free (struct hcl_number *);
+
 #endif /* LIBHCL_HCL_NUMBER_H */
--- src/hcl_number.c
+++ src/hcl_number.c
@@ -3,5 +3,22 @@
  * @brief Implementation of HCL number type
  * @author nukosuke
  */
+#include <stdlib.h>
 #include <hcl/number.h>
 
+/**
+ * Allocator of HCL number type
+ */
+struct hcl_number *hcl_number_new (void)
+{
+  struct hcl_number *num = (struct hcl_number *) malloc (sizeof (struct hcl_number));
+  return num;
+}
+
+/**
+ * Destructor of HCL number type
+ */
+void hcl_number_free (struct hcl_number *num)
+{
+  free (num);
+}
