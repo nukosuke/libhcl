7e303e6a276135b16d17d17be49c6f2f3c8f7820
nukosuke <nukosuke@lavabit.com>
2019-06-24 15:35:53 +0900

    Add comment

--- src/hcl_tuple.c
+++ src/hcl_tuple.c
@@ -6,12 +6,22 @@
 #include <stdlib.h>
 #include <hcl/tuple.h>
 
+/**
+ * Allocator of HCL tuple type
+ *
+ * @return Pointer of HCL tuple type
+ */
 struct hcl_tuple *hcl_tuple_new (void)
 {
   struct hcl_tuple *tuple = (struct hcl_tuple *) malloc (sizeof (struct hcl_tuple));
   return tuple;
 }
 
+/**
+ * Destructor of HCL tuple type
+ *
+ * @param Pointer of HCL tuple type
+ */
 void hcl_tuple_free (struct hcl_tuple *tuple)
 {
   free (tuple);
