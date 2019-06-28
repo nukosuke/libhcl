8b2b6c6decfedc0e8438faf448dc5aaecbce4462
nukosuke <nukosuke@lavabit.com>
2019-06-28 15:52:57 +0900

    Fix

--- test/test_hcl_list.c
+++ test/test_hcl_list.c
@@ -6,7 +6,7 @@
 #include <check.h>
 #include <hcl/list.h>
 
-START_TEST(test_hcl_list_alloc)
+START_TEST (test_hcl_list_alloc)
 {
   struct hcl_list *list = hcl_list_new ();
 
@@ -16,20 +16,20 @@ END_TEST
 
 Suite *hcl_list_suite (void)
 {
-  Suite *suite = suite_create("hcl_list");
-  TCase *tcase = tcase_create("alloc");
+  Suite *suite = suite_create ("hcl_list");
+  TCase *tcase = tcase_create ("alloc");
 
-  tcase_add_test(tcase, test_hcl_list_alloc);
-  suite_add_tcase(suite, tcase);
+  tcase_add_test (tcase, test_hcl_list_alloc);
+  suite_add_tcase (suite, tcase);
 
   return suite;
 }
 
 int main (void)
 {
-  SRunner *runner = srunner_create(NULL);
+  SRunner *runner = srunner_create (hcl_list_suite ());
 
-  srunner_run_all(runner, CK_NORMAL);
-  int failed = srunner_ntests_failed(runner);
+  srunner_run_all (runner, CK_NORMAL);
+  int failed = srunner_ntests_failed (runner);
   return failed;
 }
--- test/test_hcl_map.c
+++ test/test_hcl_map.c
@@ -6,27 +6,27 @@
 #include <check.h>
 #include <hcl/map.h>
 
-START_TEST(test_hcl_map_alloc)
+START_TEST (test_hcl_map_alloc)
 {
 }
 END_TEST
 
-Suite *hcl_list_suite (void)
+Suite *hcl_map_suite (void)
 {
-  Suite *suite = suite_create("hcl_map");
-  TCase *tcase = tcase_create("alloc");
+  Suite *suite = suite_create ("hcl_map");
+  TCase *tcase = tcase_create ("alloc");
 
-  tcase_add_test(tcase, test_hcl_map_alloc);
-  suite_add_tcase(suite, tcase);
+  tcase_add_test (tcase, test_hcl_map_alloc);
+  suite_add_tcase (suite, tcase);
 
   return suite;
 }
 
 int main (void)
 {
-  SRunner *runner = srunner_create(NULL);
+  SRunner *runner = srunner_create (hcl_map_suite ());
 
-  srunner_run_all(runner, CK_NORMAL);
-  int failed = srunner_ntests_failed(runner);
+  srunner_run_all (runner, CK_NORMAL);
+  int failed = srunner_ntests_failed (runner);
   return failed;
 }
