0b3acc049acd1f1e6f77af628cd0d3a243c295ba
nukosuke <nukosuke@lavabit.com>
2019-06-28 12:51:20 +0900

    Add test file for hcl_list

--- test/test_hcl_list.c
+++ test/test_hcl_list.c
@@ -3,3 +3,33 @@
  * @brief Test of HCL list type
  * @author nukosuke
  */
+#include <check.h>
+#include <hcl/list.h>
+
+START_TEST(hcl_list_new_free)
+{
+  struct hcl_list *list = hcl_list_new ();
+
+  hcl_list_free (list);
+}
+END_TEST
+
+Suite *hcl_list_suite (void)
+{
+  Suite *suite = suite_create("hcl_list");
+  TCase *tcase = tcase_create("alloc");
+
+  tcase_add_test(tcase, hcl_list_new_free);
+  suite_add_tcase(suite, tcase);
+
+  return suite;
+}
+
+int main (void)
+{
+  SRunner *runner = srunner_create(NULL);
+
+  srunner_run_all(runner, CK_NORMAL);
+  int failed = srunner_ntests_failed(runner);
+  return failed;
+}
